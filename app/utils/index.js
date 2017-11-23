17230732dcd453016bdc171e72624e4a79180c5d
Carlos Zanella <czanella@hotmail.com>
2017-11-23 11:45:58 -0200

    Adds TrailPoint class

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,7 +1,9 @@
 import randomPick from './randomPick.js';
 import CircularQueue from './CircularQueue.js';
+import TrailPoint from './TrailPoint.js';
 
 export {
     randomPick,
     CircularQueue,
+    TrailPoint,
 };
